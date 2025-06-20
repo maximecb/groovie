f1c4fa2a03179752fd8a9e7cb2a923f65782dda1
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2025-06-20 16:43:39 -0400

    Start sketching main.js

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+
+
+
+We could use more high quality samples. These should be in PCM wav format,
+and they have to be available under the CC0 license.
--- main.js
+++ main.js
@@ -1,3 +1,48 @@
+import { SAMPLE_PATHS } from "./sample_list.js";
+console.assert(SAMPLE_PATHS.length > 0);
+
+// Play pattern button
+let play_pat = document.getElementById('play_pat');
+
+// Tempo in beats per minute
+let tempo = 120;
+
+// List of patterns
+let patterns = Array(8);
+
+// Currently selected pattern
+let cur_pat = 0;
 
 
 
+
+
+
+class Pattern
+{
+    initialize()
+    {
+        this.steps_per_bar = 16;
+        this.num_bars = 1;
+        this.num_steps = this.steps_per_bar * this.num_bars;
+
+        // Initialize grid cells
+        this.rows = [];
+        for (let i = 0; i < 6; ++i)
+        {
+            this.rows[i] = Array(this.steps_per_bar * this.num_bars);
+            this.rows[i].fill(0);
+        }
+    }
+}
+
+
+
+
+play_pat.onclick = function ()
+{
+
+
+
+
+}
