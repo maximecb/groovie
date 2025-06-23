52986c88e92de50d9d5d2d52c70348935b3a58da
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2025-06-23 13:44:53 -0400

    Logic to set default sample indices and fetch default samples

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ class SampleManager
 {
     constructor()
     {
+        // Construct a mapping of sample names to indices
+        this.names_to_idxs = new Map();
+        for (let sample_idx = 0; sample_idx < SAMPLE_PATHS.length; ++sample_idx)
+        {
+            let sample_path = SAMPLE_PATHS[sample_idx];
+            let sample_name = sample_path.match(/samples\/(.+)\.wav/)[1];
+            console.assert(typeof sample_name == 'string');
+            this.names_to_idxs.set(sample_name, sample_idx);
+        }
+
         // Audio buffers for the samples
         this.sample_bufs = Array(SAMPLE_PATHS.length);
 
@@ -16,10 +26,10 @@ class SampleManager
         });
     }
 
-    // Load a sample by index
+    // Fetch/download a sample by index
     // Note that this only requests to load the sample asynchronously.
     // This function doesn't return anything
-    load_sample(sample_idx)
+    fetch_sample(sample_idx)
     {
         console.assert(sample_idx < SAMPLE_PATHS.length);
 
@@ -27,9 +37,12 @@ class SampleManager
         if (this.sample_bufs[sample_idx])
             return;
 
-        fetch(SAMPLE_PATHS[sample_idx])
+        let sample_path = SAMPLE_PATHS[sample_idx];
+        console.log(`Fetching ${sample_path}`);
+
+        fetch(sample_path)
         .then(response => response.arrayBuffer())
-        .then(array_buffer => ctx.decodeAudioData(array_buffer))
+        .then(array_buffer => audio_ctx.decodeAudioData(array_buffer))
         .then(audio_buffer => this.sample_bufs[sample_idx] = audio_buffer)
         .catch(err => console.error(err));
     }
@@ -44,6 +57,13 @@ class SampleManager
 
         return buf;
     }
+
+    // Get the index for a given sample name
+    get_idx(sample_name)
+    {
+        console.assert(this.names_to_idxs.has(sample_name));
+        return this.names_to_idxs.get(sample_name);
+    }
 }
 
 class Pattern
@@ -54,25 +74,37 @@ class Pattern
         this.num_bars = 1;
         this.num_steps = this.steps_per_bar * this.num_bars;
 
+        // Default samples
+        this.sample_idxs = [
+            samples.get_idx('kick_01'),
+            samples.get_idx('snare_01'),
+            samples.get_idx('hat_closed_01'),
+            samples.get_idx('hat_open_01'),
+            samples.get_idx('clap_01'),
+        ];
+
+        // Fetch the samples for this pattern
+        for (let sample_idx of this.sample_idxs)
+        {
+            samples.fetch_sample(sample_idx);
+        }
+
         // Initialize grid cells
-        this.rows = Array(6);
+        this.rows = Array(this.sample_idxs.length);
         for (let i = 0; i < this.rows.length; ++i)
         {
             this.rows[i] = Array(this.steps_per_bar * this.num_bars);
             this.rows[i].fill(0);
         }
-
-        // TODO: default samples
-        this.sample_idxs = Array(this.rows.length);
-        for (let i = 0; i < this.rows.length; ++i)
-        {
-            this.sample_idxs[i] = 0;
-        }
     }
 }
 
-// Audio context
-let audio_ctx = null;
+// Create AudioContext with 44.1kHz sample rate
+const audio_ctx = new AudioContext({
+    sampleRate: 44100,
+});
+
+// Global volume/gain node
 let global_gain = null;
 
 const samples = new SampleManager();
@@ -98,21 +130,17 @@ let cur_pat = 0;
 // Initialize the web audio context
 async function init_web_audio()
 {
-    if (audio_ctx)
+    if (global_gain)
         return;
 
     console.log('Initializing web audio');
-
-    // Create AudioContext with 44.1kHz sample rate
-    audio_ctx = new AudioContext({
-        sampleRate: 44100,
-    });
 
     // Global volume node
     global_gain = audio_ctx.createGain();
     global_gain.gain.setValueAtTime(volume_slider.valueAsNumber, audio_ctx.currentTime);
     global_gain.connect(audio_ctx.destination);
 
+    // The audio context starts out in a paused state
     await audio_ctx.resume();
 }
 
